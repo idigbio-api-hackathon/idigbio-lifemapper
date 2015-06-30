d5f3e04f7ac909b09467de94353060416c1d6f9e
am <ammatsun@ufl.edu>
2015-06-30 14:58:57 -0400

    Making it possible to be displayed in an iframe from another site, and handling case where species given does not have predictive experiment from Lm.

--- lm/map/views.py
+++ lm/map/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 from species.models import Species
 from constants import *
@@ -10,60 +11,66 @@ import os
 os.environ['LIFEMAPPER_CONFIG_FILE'] = '/opt/LmClient/idigbio-config.ini'
 from LmClient.lmClientLib import LMClient
 
+@xframe_options_exempt
 def index(request):
     species_list = Species.objects.order_by('species')
     context = {'species_list': species_list,}
     return render(request, 'lm/index.html', context)
 #    return HttpResponse("Choose a species:")
 
+@xframe_options_exempt
 def detail(request, species):
     species_list = Species.objects.order_by('species')
     cl = LMClient()
-    print species
+    print "Retrieving predictions for:", species
     expAtoms = cl.sdm.listExperiments(displayName=species)
-    expId = expAtoms[0].id # Get only the first experiment id
-    exp = cl.sdm.getExperiment(expId)
-    algorithmCode = exp.algorithm.code
+    prjsSummary = None
+    modelSummary = None
+    if expAtoms:
+        expId = expAtoms[0].id # Get only the first experiment id
+        exp = cl.sdm.getExperiment(expId)
+        algorithmCode = exp.algorithm.code
 
-    # Get the algorithm parameters used to build the model.  Remove items that start with an 
-    #    underscore as they are just extra object data that can be ignored
-    algorithmParameters = [(n, v) for n,v in exp.algorithm.parameters.__dict__.items() if not n.startswith('_')]
-    # Build the main mapping URL parameters (in constants.py)
-    urlParams = '&'.join(["%s=%s" % (param, val) for param, val in WMS_PARAMETERS.items()])
-    occMapUrl = "%s&%s" % (cl.sdm.getOgcEndpoint(exp.model.occurrenceSet), urlParams)
-    if INCLUDE_BACKGROUND_IMAGE:
-        occMapUrl = occMapUrl.replace('layers=', 'layers=bmng,')
-
-    # Model summary statement.  I am using a list comprehension to join the algorithm parameters
-    modelSummary = ["Experiment " + exp.id + ". Model built with algorithm: " + algorithmCode + ". Occurrences used for the model displayed on the map.", "Parameters: " + ', '.join(["%s = %s" % (p, v) for p, v in algorithmParameters]), occMapUrl]
-
-    #List of projection summaries
-    prjsSummary = [None] * len(exp.projections)
-    prjOrder = ['WC-5MIN', 'RCP8.5-CCSM4-2050', 'RCP8.5-CCSM4-2070']
-
-    # Build a projection summary tuple for each projection in the experiment
-    scenarios = {'RCP8.5-CCSM4-2050': 'Predicted 2041-2060 climate calculated from change modeled by Community Climate System Model, 4.0, National Center for Atmospheric Research (NCAR) for the IPCC Fifth Assessment Report (2013), Family RCP8.5 plus Worldclim 1.4 observed mean climate','RCP8.5-CCSM4-2070': 'Predicted 2061-2080 climate calculated from change modeled by Community Climate System Model, 4.0, National Center for Atmospheric Research (NCAR) for the IPCC Fifth Assessment Report (2013), Family RCP8.5 plus Worldclim 1.4 observed mean climate','WC-5MIN':'WorldClim 1.4 elevation and bioclimatic variables computed from interpolated observation data collected between 1950 and 2000 (http ://www.worldclim.org/), 5 min resolution'}
-    for prj in exp.projections:
-        mapUrl = "%s&%s" % (cl.sdm.getOgcEndpoint(prj), urlParams)
-
+        # Get the algorithm parameters used to build the model.  Remove items that start with an 
+        #    underscore as they are just extra object data that can be ignored
+        algorithmParameters = [(n, v) for n,v in exp.algorithm.parameters.__dict__.items() if not n.startswith('_')]
+        # Build the main mapping URL parameters (in constants.py)
+        urlParams = '&'.join(["%s=%s" % (param, val) for param, val in WMS_PARAMETERS.items()])
+        occMapUrl = "%s&%s" % (cl.sdm.getOgcEndpoint(exp.model.occurrenceSet), urlParams)
         if INCLUDE_BACKGROUND_IMAGE:
-            mapUrl = mapUrl.replace('layers=', 'layers=bmng,')
-        prjsSummary[prjOrder.index(prj.scenarioCode)] = (prj.id, prj.scenarioCode, scenarios[prj.scenarioCode], mapUrl, prj.status) 
-        #prjsSummary.append((prj.id, prj.scenarioCode, scenarios[prj.scenarioCode], mapUrl))
+            occMapUrl = occMapUrl.replace('layers=', 'layers=bmng,')
 
-    #lyrs = cl.sdm.listProjections(public=True, displayName=species)
-    #print "Number of projections:", len(lyrs)
-    #maps = []
-    #for item in lyrs:
-    #    print item.id, item.title
-    #    prj = cl.sdm.getProjection(item.id)
-    #    u = cl.sdm.getOgcEndpoint(prj)
-    #    maps.append(u)
-    #    print u
+        # Model summary statement.  I am using a list comprehension to join the algorithm parameters
+        modelSummary = ["Experiment " + exp.id + ". Model built with algorithm: " + algorithmCode + ". Occurrences used for the model displayed on the map.", "Parameters: " + ', '.join(["%s = %s" % (p, v) for p, v in algorithmParameters]), occMapUrl]
+
+        #List of projection summaries
+        prjsSummary = [None] * len(exp.projections)
+        prjOrder = ['WC-5MIN', 'RCP8.5-CCSM4-2050', 'RCP8.5-CCSM4-2070']
+
+        # Build a projection summary tuple for each projection in the experiment
+        scenarios = {'RCP8.5-CCSM4-2050': 'Predicted 2041-2060 climate calculated from change modeled by Community Climate System Model, 4.0, National Center for Atmospheric Research (NCAR) for the IPCC Fifth Assessment Report (2013), Family RCP8.5 plus Worldclim 1.4 observed mean climate','RCP8.5-CCSM4-2070': 'Predicted 2061-2080 climate calculated from change modeled by Community Climate System Model, 4.0, National Center for Atmospheric Research (NCAR) for the IPCC Fifth Assessment Report (2013), Family RCP8.5 plus Worldclim 1.4 observed mean climate','WC-5MIN':'WorldClim 1.4 elevation and bioclimatic variables computed from interpolated observation data collected between 1950 and 2000 (http ://www.worldclim.org/), 5 min resolution'}
+        for prj in exp.projections:
+            mapUrl = "%s&%s" % (cl.sdm.getOgcEndpoint(prj), urlParams)
+
+            if INCLUDE_BACKGROUND_IMAGE:
+                mapUrl = mapUrl.replace('layers=', 'layers=bmng,')
+            prjsSummary[prjOrder.index(prj.scenarioCode)] = (prj.id, prj.scenarioCode, scenarios[prj.scenarioCode], mapUrl, prj.status) 
+            #prjsSummary.append((prj.id, prj.scenarioCode, scenarios[prj.scenarioCode], mapUrl))
+
+        #lyrs = cl.sdm.listProjections(public=True, displayName=species)
+        #print "Number of projections:", len(lyrs)
+        #maps = []
+        #for item in lyrs:
+        #    print item.id, item.title
+        #    prj = cl.sdm.getProjection(item.id)
+        #    u = cl.sdm.getOgcEndpoint(prj)
+        #    maps.append(u)
+        #    print u
 
     context = {'species_list': species_list, 'species': species, 'maps': prjsSummary, 'model': modelSummary}
     return render(request, 'lm/index.html', context)
 
+@xframe_options_exempt
 def tutorial(request):
     cl = LMClient()
     scenarios = [ ['RCP8.5-CCSM4-2050', 'Predicted 2041-2060 climate calculated from change modeled by Community Climate System Model, 4.0, National Center for Atmospheric Research (NCAR) for the IPCC Fifth Assessment Report (2013), Family RCP8.5 plus Worldclim 1.4 observed mean climate'],['RCP8.5-CCSM4-2070', 'Predicted 2061-2080 climate calculated from change modeled by Community Climate System Model, 4.0, National Center for Atmospheric Research (NCAR) for the IPCC Fifth Assessment Report (2013), Family RCP8.5 plus Worldclim 1.4 observed mean climate'],['WC-5MIN','WorldClim 1.4 elevation and bioclimatic variables computed from interpolated observation data collected between 1950 and 2000 (http ://www.worldclim.org/), 5 min resolution'] ]
